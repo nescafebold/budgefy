1e7900d82efdc3728ea55cc49249a1584236b6aa
b4pnu <b4pnupuas@gmail.com>
2026-04-15 16:54:10 +0800

    added utils only

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,19 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatCurrency(amount: number, currency = "PHP") {
+  return new Intl.NumberFormat("en-PH", {
+    style: "currency",
+    currency,
+    minimumFractionDigits: 2,
+  }).format(amount)
+}
+
+export function formatDate(date: string | Date) {
+  return new Intl.DateTimeFormat("en-PH", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  }).format(new Date(date))
+}
