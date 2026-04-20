3612159278e61ec06c0bcf13ad87b32c70420772
b4pnu <b4pnupuas@gmail.com>
2026-04-20 16:48:45 +0800

    changed db/index.ts

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,6 +1,7 @@
 // src/lib/db/index.ts
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
+import * as schema from "@/lib/db/schema"
 
 const sql = neon(process.env.DATABASE_URL!);
-export const db = drizzle(sql);
+export const db = drizzle(sql, { schema });
